9c6c50e720f9bb8d67a2ed386e49eaee67ef1dac
Moonnigt7 <katha.w92@gmx.at>
2021-06-21 09:09:39 +0200

    forecast

--- soap.php
+++ soap.php
@@ -105,41 +105,42 @@ echo ' Current Weather ' . $data . ' Grad';
     <section>
         <h2>7 Day Forecast</h2>
         <?php
-        $url = "http://localhost:5000/ws";
 
-        $request_param_forecast = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            $url = "http://localhost:5000/ws";
+
+            $request_param_forecast = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
                                             xmlns:gs="http://vs_project.fh/models/soap/weather">
                             <soapenv:Header/>
                                  <soapenv:Body>
-                                    <gs:getCurrentWeatherRequest/>
+                                    <gs:getForecastRequest/>
                                 </soapenv:Body>
                          </soapenv:Envelope>';
 
-        $headers = array(
-        'Content-Type: text/xml; charset=utf-8',
-        'Content-Length: '.strlen($request_param_forecast)
-        );
+            $headers = array(
+                'Content-Type: text/xml; charset=utf-8',
+                'Content-Length: ' . strlen($request_param_forecast)
+            );
 
-        $ch = curl_init($url);
-        curl_setopt ($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt ($ch, CURLOPT_POSTFIELDS, $request_param_forecast);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $request_param_forecast);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        $data = curl_exec ($ch);
+            $data = curl_exec($ch);
 
-        $result = $data;
+            $result = $data;
 
-        if ($result === FALSE) {
-        printf("CURL error (#%d): %s<br>\n", curl_errno($ch),
-        htmlspecialchars(curl_error($ch)));
-        }
+            if ($result === FALSE) {
+                printf("CURL error (#%d): %s<br>\n", curl_errno($ch),
+                    htmlspecialchars(curl_error($ch)));
+            }
 
-        curl_close ($ch);
+            curl_close($ch);
 
-        echo ' Forecast ' . $data ;
+            echo ' Forecast ' . $data.'<br>';
 
         ?>
 
